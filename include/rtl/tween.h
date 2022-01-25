899b3b62766ad531fff9cee159350333918dfadc
Lorents Odin Gravås <lorents@movi.ai>
2022-01-25 10:31:09 +0100

    Add tween constructor overload without delay

--- include/rtl/tween.h
+++ include/rtl/tween.h
@@ -15,6 +15,17 @@ namespace rtl
 		std::function<void()> completed;
 
 		tween(
+				double duration,
+				easing::function easing,
+				std::function<void()> completed
+		):
+				duration(duration),
+				easing(easing),
+				delay(0.0),
+				completed(completed)
+		{}
+
+		tween(
 			double duration = 0.0,
 			easing::function easing = easing::linear,
 			double delay = 0.0,
